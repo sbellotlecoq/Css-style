db86b79bad96a43f2ba531a75df63687e5ec9a77
sbellotlecoq <sbellotlecoq@gmail.com>
2023-02-28 09:36:29 +0100

    formulaire et parallax index

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :previews
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+    root "previews#index"
   # Defines the root path route ("/")
   # root "articles#index"
 end
